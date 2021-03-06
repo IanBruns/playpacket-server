341a5cefd52689e11c7e9b2c062259c1a7ba7876
Ian Bruns <ian@rbruns.com>
2021-03-06 15:57:14 -0700

    added empty post route

--- src/rules/rules-router.js
+++ src/rules/rules-router.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const RulesService = require('./rules-service');
 const { requireAuth } = require('../middleware/api-auth');
-
 const rulesRouter = express.Router();
 const jsonBodyParser = express.json();
 
@@ -13,6 +12,9 @@ rulesRouter.route('/')
                 return res.json(rules.map(RulesService.sanitizeUserRule));
             })
             .catch(next);
+    })
+    .post(jsonBodyParser, (req, res, next) => {
+
     })
 
 module.exports = rulesRouter;
