5a767accd970a87912edfd1f4acfdbc000312978
Ian Bruns <ian@rbruns.com>
2021-03-10 16:46:43 -0700

    all User validaion works with new db

--- test/games-endpoints.spec.js
+++ test/games-endpoints.spec.js
@@ -4,7 +4,7 @@ const supertest = require('supertest');
 const app = require('../src/app');
 const helpers = require('./test-helpers');
 
-describe('/api/games endpoints', () => {
+describe.only('/api/games endpoints', () => {
     let db;
 
     const { testUsers, testGames, testRules } = helpers.makePlayPacketFixtures();
